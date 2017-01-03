7ccd573a3920d8e6d2d5e50216c955ce548022dc
thomasa19 <thomas.arnqvist@outlook.com>
2017-01-03 14:43:05 +0100

    Added sample class in comments

--- Affarssystemet/Customer.cs
+++ Affarssystemet/Customer.cs
@@ -8,5 +8,29 @@ namespace Affarssystemet
 {
     class Customer
     {
+
     }
+
+    /*
+    class Customer
+    {
+        public int customerNumberTA { get; private set; }
+        public string customerNameTA { get; private set; }
+        public string customerAddressTA { get; private set; }
+
+        public Customer(int custNo, string custName, string custAddress)
+        {
+            customerNumberTA = custNo;
+            customerNameTA = custName;
+            customerAddressTA = custAddress;
+        }
+
+        public override string ToString()
+        {
+            return "Kundnummer: " + customerNumberTA + "\n" +
+                   "Namn:       " + customerNameTA + "\n" +
+                   "Adress:     " + customerAddressTA + "\n";
+        }
+    }
+    */
 }
--- Affarssystemet/Product.cs
+++ Affarssystemet/Product.cs
@@ -8,5 +8,32 @@ namespace Affarssystemet
 {
     class Product
     {
+
     }
+
+    /* 
+    class Product
+    {
+        public int productNumberTA { get; private set; }
+        public string productNameTA { get; private set; }
+        public decimal productPriceTA { get; private set; }
+        public int productsInStorageTA { get; private set; }
+
+        public Product(int prodNo, string prodName, decimal prodPrice, int prodInStorage)
+        {
+            productNumberTA = prodNo;
+            productNameTA = prodName;
+            productPriceTA = prodPrice;
+            productsInStorageTA = prodInStorage;
+        }
+
+        public override string ToString()
+        {
+            return "Artikelnummer: " + productNumberTA + "\n" +
+                   "Benämning:     " + productNameTA + "\n" +
+                   "Pris:          " + productPriceTA.ToString("C") + "\n" +
+                   "Antal i lager: " + productsInStorageTA + "\n";
+        }
+    }
+    */
 }
